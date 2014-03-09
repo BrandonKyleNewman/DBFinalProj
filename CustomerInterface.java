9add86fb52963f1c20eed95b9d834fcb291a2160
Brandon Newman <bnewman@umail.ucsb.edu>
2014-03-09 01:37:26 -0800

    Added prompts to Customer interface

--- CustomerInterface.java
+++ CustomerInterface.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class CustomerInterface
 {
@@ -7,8 +8,46 @@ public class CustomerInterface
 	private ConnectionHandler custConn;
 
 
-	private void search(String searchItem)
+	private void search(String[] searchItem)
 	{
+		//for every item in search item
+			//split it into attribute and identifier
+			//add to query
+		//once finished, search query
+	}
+
+	private void searchPrompt()
+	{
+		ArrayList<String> searches = new ArrayList<String>();
+
+		System.out.println("Search in the form 'attribute identifier'. Press 'done' when finished with searches.");
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean continueSearch = true;
+		int stringCounter = 0;
+		String searchQuery = null;
+
+		while (continueSearch == true)
+		{
+			try
+			{
+				searchQuery = br.readLine();
+			}
+			catch (IOException ioe)
+			{
+				System.out.println("Fuck");
+			}
+			if (!searchQuery.equals("done"))
+			{
+				searches.add(searchQuery);
+				stringCounter++;
+			}
+			else
+			{
+				System.out.println("See yA!");
+				continueSearch = false;
+			}
+		}
 
 	}
 
@@ -64,6 +103,31 @@ public class CustomerInterface
 			catch (IOException ioe)
 			{
 				System.out.println("Fuck");
+			}
+
+			switch( command )
+			{
+				case "search":
+					searchPrompt();
+				case "add":
+					break;
+				case "delete":
+					break;
+				case "display":
+					break;
+				case "check out":
+					break;
+				case "find":
+					break;
+				case "re-run":
+					break;
+				case "quit":
+					continueAccess = false;
+					break;
+				case "help":
+					break;
+				default:
+					System.out.println("Unrecognized command. Enter help for list of commands");
 			}
 		}
 		
